75d69564b5711c1082eeb45abeefb9efd9869b3e
TDR <tdr.minecraft@gmail.com>
2023-08-08 18:17:39 +0200

    Introduce Batches

---
+++ Ingwer Landfill/Server/src/main/java/de/bentzin/ingwer/landfill/db/user/badge/Badge.java
@@ -0,0 +1,16 @@
+package de.bentzin.ingwer.landfill.db.user.badge;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ * @author Ture Bentzin
+ * @since 2023-08-06
+ */
+@Entity
+@Table(name = "badge")
+public class Badge {
+
+
+
+}
